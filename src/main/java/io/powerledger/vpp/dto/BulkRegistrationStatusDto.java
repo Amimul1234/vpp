5487909f75e05a9da16c4e0642b553dd2f125f4b
Amimul1234 <amimulehsan56@gmail.com>
2025-01-17 16:30:51 +0600

    Need to check on kafka for serialization error.

--- src/main/java/io/powerledger/vpp/dto/BulkRegistrationStatusDto.java
+++ src/main/java/io/powerledger/vpp/dto/BulkRegistrationStatusDto.java
@@ -2,16 +2,19 @@ package io.powerledger.vpp.dto;
 
 import io.powerledger.vpp.enums.BatteryRegistrationStatus;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Data
+@NoArgsConstructor
 public class BulkRegistrationStatusDto {
     private BatteryRegistrationStatus status;
     private int totalBatches;
     private int failedBatches;
     private String bulkRequestId;
     private int completedBatches;
-    private Map<String, List<BatteryDto>> failedBatchDetails;
+    private Map<String, List<BatteryDto>> failedBatchDetails = new HashMap<>();
 }
